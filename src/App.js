2994a25e8ae3959cfd6d2895920b9e151ac3b003
Peter Onneby <ponneby@xebia.fr>
2017-12-26 23:04:05 +0100

    Improved positioning

--- src/App.js
+++ src/App.js
@@ -21,29 +21,30 @@ class App extends Component {
 
   render() {
     return (
-      <svg x="0px" y="0px" viewBox="0 0 1000 1000">
+      <svg viewBox="0 0 1000 1000">
       <defs>
-      <filter id="B4" x="-150%" width="400%" y="-150%" height="400%">
-      <feOffset in="SourceGraphic" result="pre-red" dx="0" dy={this.state.dy*-1} />
-      <feColorMatrix in="pre-red" type="matrix" result="red" values="0 0 0 0 1
-                                                                     0 0 0 0 0
-                                                                     0 0 0 0 0
-                                                                     0 0 0 1 0"/>
-      <feOffset in="SourceGraphic" result="pre-blue" dx="0" dy={this.state.dy} />
-      <feColorMatrix in="pre-blue" type="matrix" result="blue" values="0 0 0 0 0
-                                                                       0 0 0 0 0
-                                                                       0 0 0 0 1
-                                                                       0 0 0 1 0"/>
-      <feBlend mode="screen" in="red" in2="blue" result="main"/>
-      <feBlend mode="screen" in="main" in2="SourceGraphic"/>
-      </filter>
+        <filter id="B4" x="-150%" width="400%" y="-150%" height="400%">
+          <feOffset in="SourceGraphic" result="pre-red" dx="0" dy={this.state.dy*-1} />
+          <feColorMatrix in="pre-red" type="matrix" result="red" values="0 0 0 0 1
+                                                                         0 0 0 0 0
+                                                                         0 0 0 0 0
+                                                                         0 0 0 1 0"/>
+          <feOffset in="SourceGraphic" result="pre-blue" dx="0" dy={this.state.dy} />
+          <feColorMatrix in="pre-blue" type="matrix" result="blue" values="0 0 0 0 0
+                                                                           0 0 0 0 0
+                                                                           0 0 0 0 1
+                                                                           0 0 0 1 0"/>
+          <feBlend mode="screen" in="red" in2="blue" result="main"/>
+          <feBlend mode="screen" in="main" in2="SourceGraphic"/>
+        </filter>
       </defs>
       <text
         filter="url(#B4)"
-        className="path"
         id="svg_1"
-        x="200"
-        y="220"
+        x="50%"
+        y="30%"
+        alignment-baseline="middle"
+        text-anchor="middle"
         fill="#00FF00">ONNEBY.COM</text>
     </svg>);
   }
